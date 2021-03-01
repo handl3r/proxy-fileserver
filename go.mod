234c6763380fff265cf486a82ea3bf6ed206b26e
handl3r <thaibuixuan34@gmail.com>
2021-03-02 00:56:04 +0700

    Add google drive service

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/joho/godotenv v1.3.0
 	go.uber.org/zap v1.16.0
+	google.golang.org/api v0.40.0
 )
---
+++ src/adapter/services/google_drive_service.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"context"
+	"google.golang.org/api/drive/v3"
+)
+
+type GoogleDriveService struct {
+	service *drive.Service
+	sharedFolder string
+}
+
+func NewGoogleDriveClient(ctx context.Context, sharedFolder string) (*GoogleDriveService, error){
+	service, err := drive.NewService(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &GoogleDriveService{
+		service: service,
+		sharedFolder: sharedFolder,
+	}, nil
+}
+
+func GetFile() {
+
+}
