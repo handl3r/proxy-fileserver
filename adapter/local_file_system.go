2ae554c9af0525426f5f0b5ecd2e0911c1bd9330
Thang Bui <zenthangplus@gmail.com>
2021-03-11 22:00:51 +0700

    Change file permission

--- adapter/local_file_system.go
+++ adapter/local_file_system.go
@@ -53,7 +53,7 @@ func (s *LocalFileSystem) NewFile(filePath string) (io.Writer, error) {
 	if len(files) > 1 {
 		lastFile = files[len(files)-1]
 		directory = strings.Join(files[0:len(files)-1], "/")
-		err := os.MkdirAll(s.rootFolder+"/"+directory, 0777)
+		err := os.MkdirAll(s.rootFolder+"/"+directory, 0775)
 		if err != nil {
 			return nil, err
 		}
