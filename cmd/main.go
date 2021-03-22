8db9cb8affd54421eaadfe92e482a627aa66fb8c
thaibx <thai.bui1@onemount.com>
2021-03-22 23:57:35 +0700

    feat/add prelaod lock

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"proxy-fileserver/common/lock"
 	"proxy-fileserver/common/log"
 	"proxy-fileserver/configs"
+	"proxy-fileserver/jobs"
 	"proxy-fileserver/models"
 )
 
@@ -34,8 +35,10 @@ func main() {
 	ctx := context.Background()
 	appContext := bootstrap.InitService(ctx, dbConnection)
 
+	jobs.LoadLockFromDB(appContext.AppContext.RepoProvider.GetFileInfoRepository())
+
 	c := cron.New()
-	cleaner := api.NewCleaner(appContext.AppContext.RepoProvider.GetFileInfoRepository(), configs.Get().CacheTimeLocalFileSystem,
+	cleaner := jobs.NewCleaner(appContext.AppContext.RepoProvider.GetFileInfoRepository(), configs.Get().CacheTimeLocalFileSystem,
 		configs.Get().SharedRootFolder, appContext.AppContext.AdapterProvider.GetLocalFileSystem())
 	_ = c.AddFunc(fmt.Sprintf("@every %dm", conf.CycleTimeCleaner), cleaner.Run)
 	c.Start()
--- jobs/cronjob.go
+++ jobs/cronjob.go
@@ -1,4 +1,4 @@
-package api
+package jobs
 
 import (
 	"proxy-fileserver/adapter"
